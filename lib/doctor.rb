39a8f3d040c80e6f4ac815aab081486901ab2498
Jenise Ruehle <jela07@gmail.com>
2020-03-23 17:59:17 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end 
   
   def patients
-    appointments.map {|appointment| appointment.patient}
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
   
   def new_appointment(date, patient)
